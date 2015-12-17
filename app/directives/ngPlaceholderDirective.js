23328b2bd25a00216f307339d465020b3d0253c9
HappyNinja <denis@tractive.com.my>
2015-12-17 18:35:10 +0800

    select floating label

--- app/directives/ngPlaceholderDirective.js
+++ app/directives/ngPlaceholderDirective.js
@@ -2,11 +2,16 @@ angular
     .module('app.core')
     .directive('ngPlaceholder', ngPlaceholder);
 
-function ngPlaceholder() {
+function ngPlaceholder($interpolate, $parse) {
     var linkFunction = function(scope, element, attributes, ngModel) {
         //Create a copy of the original data that’s passed in
         function init() {
             var formGroup = element.parent();
+            if(element.is("select")){
+                var html = '<label for="' + attributes.name + '">' + attributes.ngPlaceholder.split("{}")[0] + '</label>';
+                formGroup.prepend(html);
+                return;
+            }
             /*var html = '<div class="">' + attributes.ngPlaceholder.split("{}")[1] + '</div>';
             element.parent().prepend(html);*/
             var html = '<label for="' + attributes.name + '">' + attributes.ngPlaceholder.split("{}")[0] + '</label>';
@@ -34,6 +39,21 @@ function ngPlaceholder() {
                 if (ngModel.$viewValue)
                     formGroup.removeClass("is-empty");
             });
+
+            if (attributes.compareTo !== undefined) {
+                /*var pwdToMatch = $parse(attributes.compareTo);
+                var pwdFn = $interpolate(attributes.compareTo)(scope);
+                console.log(pwdFn);
+                scope.$watch(pwdFn, function(newVal) {
+                    ngModel.$setValidity('comparison', ngModel.$viewValue == newVal);
+                })
+
+                ngModel.$validators.comparison = function(modelValue, viewValue) {
+                    var value = modelValue || viewValue;
+                    return value == pwdToMatch(scope);
+                };*/
+
+            }
         }
         function transform (formGroup,ngModel){
             if (ngModel.$valid){
